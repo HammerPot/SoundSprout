3f5d6eb13645f2a646fccfb6a82ea046a0e95fea
HammerPot <potter@hammerpot.dev>
2026-04-09 13:47:52 -0400

    Base Site Done!
    Shout out to Hack Club's CSS Template

---
+++ src/routes/+page.svelte
@@ -0,0 +1,67 @@
+<a href="https://hackclub.com/">
+	<img class=" w-full max-w-32 border-0" src="https://assets.hackclub.com/flag-orpheus-left.svg" alt="Hack Club" style=" padding-top: var(--spacing-2);"/>
+</a>
+
+<div class="flex flex-col items-center " style="padding-top: var(--spacing-2); padding-bottom: var(--spacing-4);">
+	<h1 class="text-center title" style="color: var(--color-primary);"> SoundSprout </h1>
+	<p class="text-center headline" style="color: var(--color-text);"> Make a Website that Generates Music, Get Seeds to Grow a Garden! </p>
+</div>
+
+<div style="padding-top: var(--spacing-2); padding-bottom: var(--spacing-2);">
+		<div class="container">
+		<div class="card" style="background-color: var(--color-background-3);">
+			<h2 class="text-center eyebrow"> What is SoundSprout? </h2>
+			<p class="text-center" style="color: var(--color-text);"> This YSWS requires you to create a website that generates and plays music in some way. </p>
+		</div>
+	</div>
+</div>
+
+<div style="padding-top: var(--spacing-2); padding-bottom: var(--spacing-2);">
+	<div class="container">
+		<div class="card" style="background-color: var(--color-background-3);">
+			<h2 class="text-center eyebrow"> Wait, how? </h2>
+			<p class="text-center" style="color: var(--color-text);"> The web browser is a powerful tool, and there are many resources available for beginners to learn how to start creating! </p>	
+			<p class="text-center" style="color: var(--color-text);">A great resource you may find useful are the <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank">MDN Web Docs</a>, where you can find in-depth explanations of the APIs and useful libraries that can be used to achieve your goal. </p>
+		</div>
+	</div>
+</div>
+
+<!-- <div style="padding-top: var(--spacing-2); padding-bottom: var(--spacing-2);">
+	<div class="container">
+		<div class="card" style="background-color: var(--color-background-3);">
+			<h2 class="text-center eyebrow"> What resources? </h2>
+			<p class="text-center" style="color: var(--color-text);"> Whether you are a beginner or an expert, the <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank">MDN Web Docs</a> may provide a good launchpad to learn about relevant APIs. </p>
+		</div>
+	</div>
+</div> -->
+
+<div style="padding-top: var(--spacing-2); padding-bottom: var(--spacing-2);">
+	<div class="container">
+		<div class="card" style="background-color: var(--color-background-3);">
+			<h2 class="text-center eyebrow"> What do I get for submitting? </h2>
+			<p class="text-center" style="color: var(--color-text);">Ship a project with at least 6 hours of work, and you will receive a gardening kit to start growing your own garden!  </p>
+		</div>
+	</div>
+</div>
+
+<div style="padding-top: var(--spacing-4); padding-bottom: var(--spacing-4);">
+	<div class="container">
+		<div class="card" style="background-color: var(--color-background-3);">
+			<h2 class="text-center eyebrow"> Get Started </h2>
+			<div class="grid min-w-0 grid-cols-1 gap-4 md:grid-cols-3">
+				<div class="card interactive" style="">
+					<h2 class="text-center heading" style="color: var(--color-accent-text)"> Step 1: </h2>
+					<p class="text-center" style="color: var(--color-accent-text)"> Create a new website that generates music! </p>
+				</div>
+				<div class="card interactive" style="">
+					<h2 class="text-center heading" style="color: var(--color-accent-text)"> Step 2: </h2>
+					<p class="text-center" style="color: var(--color-accent-text)"> Submit your website to the SoundSprout YSWS! </p>
+				</div>
+				<div class="card interactive" style="">
+					<h2 class="text-center heading" style="color: var(--color-accent-text)"> Step 3: </h2>
+					<p class="text-center" style="color: var(--color-accent-text)"> Get mailed seeds to grow your own muse! </p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
